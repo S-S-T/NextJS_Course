2b735da635d48b64d0386b5ef9c07553ed43ce1d
Sanford Schaffer <sschaffer.ctr@kr.af.mil>
2020-11-28 07:59:19 -0500

    randomness no more, a new func

--- pages/api/random_num.ts
+++ pages/api/random_num.ts
@@ -2,8 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
 
-  res.json({ status: 'ok', num: Math.random() * .00001 });
-  req.body = { string: "husker du" };
+  res.send('somethinglikeths')
+  console.log('request body', req.body)
+  res.json({ status: 'ok', num: Math.floor(Math.random() * 1000) });
+  //req.body = { string: "husker du" };
 }
 
- // http://localhost:3000/api/randomnumber....
+ // http://localhost:3000/api/randomnumber...
